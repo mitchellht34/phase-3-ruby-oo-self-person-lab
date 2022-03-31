ef7d9e489bcb596d19b76350353e80c36382ee33
Mitch Toran <mitchellht34@gmail.com>
2022-03-31 16:37:42 -0400

    commit tree 6f405484c1abd317b2a2268000b6b1dafb50fb72

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,76 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize name
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        if value < 0
+            value = 0
+        elsif value > 10
+            value = 10
+        end
+        @happiness = value
+    end
+    
+    def hygiene=(value)
+        if value < 0
+            value = 0
+        elsif value > 10
+            value = 10
+        end
+        @hygiene = value
+    end
+
+    def clean?
+        self.hygiene > 7 ? true : false
+    end
+    
+    def happy?
+        self.happiness > 7 ? true : false
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            response = ["partisan", "lobbyist"]
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            response = ["sun", "rain"]
+        else
+            response = ["blah", "blah"]
+        end
+
+        "blah blah #{response[0]} blah #{response[1]}"
+    end
+end
